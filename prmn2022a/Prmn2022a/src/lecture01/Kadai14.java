c66cd8e937dd324d0d77184bf66da493c6f8d958
Soltlooking <b2211220@photon.chitose.ac.jp>
2022-11-06 16:15:43 +0900

    課題1-4 了

---
+++ prmn2022a/Prmn2022a/src/lecture01/Kadai14.java
@@ -0,0 +1,15 @@
+package lecture01;
+
+public class Kadai14 {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        int sum=0;
+        for(int i=0;i<100;i++){
+            array[i]=i+1;
+            if(array[i]%2==0){
+                sum=sum+array[i];
+            }
+        }
+        System.out.println("偶数和は "+sum);
+    }
+}
